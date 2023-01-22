adaf83466fffd6cc703a0d0ed053559ee0c78b41
Kirill Zagul <zagulkirill@gmail.com>
2023-01-22 22:16:56 +0500

    init flowbite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -50,5 +50,15 @@ export default <Partial<Config>>{
         }
       }
     },
-    plugins: [],
+    plugins: [
+      require('flowbite')
+    ],
+    content: [
+      "./components/**/*.{js,vue,ts}",
+      "./layouts/**/*.vue",
+      "./pages/**/*.vue",
+      "./plugins/**/*.{js,ts}",
+      "./nuxt.config.{js,ts}",
+      "./node_modules/flowbite.{js,ts}"
+    ],
   };
