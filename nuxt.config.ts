762415d112bb944b95b8804e26b348e81ce13073
Kirill Zagul <zagulkirill@gmail.com>
2023-03-04 22:24:29 +0500

    added nuxt-viewport

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        '@nuxtjs/tailwindcss'
+        '@nuxtjs/tailwindcss',
+        [
+            'nuxt-viewport', {
+                /* Viewport options */
+            }
+        ]
     ],
     css: ['~/assets/css/main.css']
 })
