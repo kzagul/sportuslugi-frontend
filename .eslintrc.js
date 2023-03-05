846dad00cfd80a8f7d40b384e19a3f3b6bfe501b
Kirill Zagul <zagulkirill@gmail.com>
2023-03-05 22:09:49 +0500

    added linter + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,38 @@
-// .eslintrc.js 
+// .eslintrc.js
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-    },
-    extends: [
-      "plugin:@typescript-eslint/recommended",
-      "plugin:nuxt/recommended",
-      "plugin:vue/vue3-recommended",
-    ],
-    parserOptions: {
-      ecmaVersion: "latest",
-      parser: "@typescript-eslint/parser",
-      sourceType: "module",
-    },
-    plugins: ["@typescript-eslint"],
-    rules: {
-      'vue/multi-word-component-names': 'off'
-    },
-  };
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
+  rules: {},
+};
+
+// module.exports = {
+//   root: true,
+//   env: {
+//     browser: true,
+//     es2021: true,
+//     node: true,
+//   },
+//   extends: [
+//     "plugin:@typescript-eslint/recommended",
+//     "plugin:nuxt/recommended",
+//     "plugin:vue/vue3-recommended",
+//   ],
+//   parserOptions: {
+//     ecmaVersion: "latest",
+//     parser: "@typescript-eslint/parser",
+//     sourceType: "module",
+//   },
+//   plugins: ["@typescript-eslint"],
+//   rules: {
+//     'vue/multi-word-component-names': 'off'
+//   },
+// };
