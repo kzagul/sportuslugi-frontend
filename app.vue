3afdad72a8c86e7bf40fa022b72345de754dd18f
Kirill Zagul <zagulkirill@gmail.com>
2023-05-02 00:42:45 +0500

    added SEO & icons

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <Html>
+    <Head>
+      <Title>{{ title }}</Title>
+      <Meta name="description" content="Агрегатор спортивных услуг" />
+    </Head>
+    <Body>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </Body>
+  </Html>
+</template>
+
+<script setup>
+const title = ref("Спорт-услуги");
+</script>
